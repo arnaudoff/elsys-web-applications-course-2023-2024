a6f103b5420121253e9551acf51a0718bd8b4e8d
Ivan Georgiev <71922026+IvanGeorgievProgramming@users.noreply.github.com>
2024-01-24 12:01:30 +0200

    Add files via upload

---
+++ src/main/java/com/example/application/Application.java
@@ -0,0 +1,16 @@
+package com.example.application;
+
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.server.PWA;
+import com.vaadin.flow.theme.Theme;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@Theme(value = "flowcrmtutorial")
+public class Application implements AppShellConfigurator {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/application/data/ContactRepository.java
@@ -0,0 +1,12 @@
+package com.example.application.data;
+
+import com.example.application.data.Contact;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface ContactRepository extends JpaRepository<Contact, Long> {
+    @Query("select c from Contact c " + "where lower(c.firstName) like lower(concat('%', :searchTerm, '%')) " + "or lower(c.lastName) like lower(concat('%', :searchTerm, '%'))") List<Contact> search(@Param("searchTerm") String searchTerm);
+}
